0ecf126c9218e6970bdd5150467af64405c95adb
oficialharshit <oficialharshit@gmail.com>
2026-08-23 16:19:08 +0530

    Time: 0 ms (100%), Space: 12.3 MB (57.98%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        index = -1
+        n = len(nums)
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                index = i
+                break
+        if index == -1:
+            nums.reverse()
+            return nums
+        for i in range(n-1,index-1,-1):
+            if nums[i]>nums[index]:
+                nums[i] , nums[index] = nums[index] , nums[i]
+                break
+        nums[index + 1:] = nums[index + 1:][::-1]
+        return nums
+
