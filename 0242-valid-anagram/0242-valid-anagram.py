3ecc8fc10ca63c0f6c97eed63371436e096d0e53
oficialharshit <oficialharshit@gmail.com>
2026-09-20 22:40:24 +0530

    Time: 24 ms (33.36%), Space: 14.3 MB (13.86%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        n = len(s)
+        count = [0]*26
+        for i in range(n):
+            count[ord(s[i])-ord('a')]+=1
+            count[ord(t[i])-ord('a')]-=1
+
+        return all(x==0 for x in count)
+
